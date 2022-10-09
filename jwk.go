36b922e092f7a876af3f679f7925c7aa5ca5b636
Micah Parks <gpg@micahparks.com>
2022-10-08 23:17:15 -0400

    Seperate JWK marshal to its own exported function

--- jwk.go
+++ jwk.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rsa"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/big"
 )
@@ -21,6 +22,9 @@ const (
 	// KeyTypeOct is the key type for octet sequences, such as HMAC.
 	KeyTypeOct KeyType = "oct"
 )
+
+// ErrUnsupportedKeyType is an error indicating a key type is not supported.
+var ErrUnsupportedKeyType = errors.New("unsupported key type")
 
 // KeyType is a set of "JSON Web Key Types" from https://www.iana.org/assignments/jose/jose.xhtml as mentioned in
 // https://www.rfc-editor.org/rfc/rfc7517#section-4.1
@@ -56,7 +60,7 @@ type OtherPrimes struct {
 // https://www.rfc-editor.org/rfc/rfc7517
 // https://www.rfc-editor.org/rfc/rfc7518
 // https://www.rfc-editor.org/rfc/rfc8037
-type jwkMarshal struct {
+type JWKMarshal struct {
 	CRV string        `json:"crv,omitempty"` // https://www.rfc-editor.org/rfc/rfc7518#section-6.2.1.1 and https://www.rfc-editor.org/rfc/rfc8037.html#section-2
 	D   string        `json:"d,omitempty"`   // https://www.rfc-editor.org/rfc/rfc7518#section-6.3.2.1 and https://www.rfc-editor.org/rfc/rfc7518#section-6.2.2.1 and https://www.rfc-editor.org/rfc/rfc8037.html#section-2
 	DP  string        `json:"dp,omitempty"`  // https://www.rfc-editor.org/rfc/rfc7518#section-6.3.2.4
@@ -74,9 +78,9 @@ type jwkMarshal struct {
 	Y   string        `json:"y,omitempty"`   // https://www.rfc-editor.org/rfc/rfc7518#section-6.2.1.3
 }
 
-// JWKSetMarshal is used to marshal or unmarshal a JSON Web Key Set.
-type JWKSetMarshal struct {
-	Keys []jwkMarshal `json:"keys"`
+// JWKSMarshal is used to marshal or unmarshal a JSON Web Key Set.
+type JWKSMarshal struct {
+	Keys []JWKMarshal `json:"keys"`
 }
 
 // JWKSet is a set of JSON Web Keys.
@@ -93,8 +97,10 @@ func NewMemory() JWKSet {
 
 // JSON creates the JSON representation of the JWKSet.
 func (j JWKSet) JSON(ctx context.Context) (json.RawMessage, error) {
-	jwks := JWKSetMarshal{}
-	encodePrivate := false
+	jwks := JWKSMarshal{}
+	options := KeyMarshalOptions{
+		EncodePrivate: false,
+	}
 
 	keys, err := j.Store.SnapshotKeys(ctx)
 	if err != nil {
@@ -102,71 +108,82 @@ func (j JWKSet) JSON(ctx context.Context) (json.RawMessage, error) {
 	}
 
 	for _, meta := range keys {
-		jwk := jwkMarshal{}
-		switch meta.Key.(type) {
-		case *ecdsa.PrivateKey:
-			key := meta.Key.(*ecdsa.PrivateKey)
-			pub := key.PublicKey
-			jwk.CRV = pub.Curve.Params().Name
-			jwk.X = bigIntToBase64RawURL(pub.X)
-			jwk.Y = bigIntToBase64RawURL(pub.Y)
-			jwk.KTY = KeyTypeEC.String()
-			if encodePrivate {
-				jwk.D = bigIntToBase64RawURL(key.D)
+		jwk, err := KeyMarshal(meta, options)
+		if err != nil {
+			if errors.Is(err, ErrUnsupportedKeyType) {
+				// Ignore the key.
+				continue
 			}
-		case ecdsa.PublicKey:
-			key := meta.Key.(ecdsa.PublicKey)
-			jwk.CRV = key.Curve.Params().Name
-			jwk.X = bigIntToBase64RawURL(key.X)
-			jwk.Y = bigIntToBase64RawURL(key.Y)
-			jwk.KTY = KeyTypeEC.String()
-		case ed25519.PrivateKey:
-			key := meta.Key.(ed25519.PrivateKey)
-			pub := key.Public().(ed25519.PublicKey)
-			jwk.X = base64.RawURLEncoding.EncodeToString(pub)
-			jwk.KTY = KeyTypeOKP.String()
-			if encodePrivate {
-				jwk.D = base64.RawURLEncoding.EncodeToString(key)
-			}
-		case ed25519.PublicKey:
-			key := meta.Key.(ed25519.PublicKey)
-			jwk.X = base64.RawURLEncoding.EncodeToString(key)
-			jwk.KTY = KeyTypeOKP.String()
-		case *rsa.PrivateKey:
-			key := meta.Key.(*rsa.PrivateKey)
-			pub := key.PublicKey
-			jwk.E = bigIntToBase64RawURL(big.NewInt(int64(pub.E)))
-			jwk.N = bigIntToBase64RawURL(pub.N)
-			jwk.KTY = KeyTypeRSA.String()
-			if encodePrivate {
-				jwk.D = bigIntToBase64RawURL(key.D)
-				jwk.P = bigIntToBase64RawURL(key.Primes[0])
-				jwk.Q = bigIntToBase64RawURL(key.Primes[1])
-				jwk.DP = bigIntToBase64RawURL(key.Precomputed.Dp)
-				jwk.DQ = bigIntToBase64RawURL(key.Precomputed.Dq)
-				jwk.QI = bigIntToBase64RawURL(key.Precomputed.Qinv)
-				for i := 2; i < len(key.Primes); i++ {
-					jwk.OTH = append(jwk.OTH, OtherPrimes{
-						CRTFactorExponent:    bigIntToBase64RawURL(key.Precomputed.CRTValues[i].Exp),
-						CRTFactorCoefficient: bigIntToBase64RawURL(key.Precomputed.CRTValues[i].Coeff),
-						PrimeFactor:          bigIntToBase64RawURL(key.Precomputed.CRTValues[i].R),
-					})
-				}
-			}
-		case rsa.PublicKey:
-			key := meta.Key.(rsa.PublicKey)
-			jwk.E = bigIntToBase64RawURL(big.NewInt(int64(key.E)))
-			jwk.N = bigIntToBase64RawURL(key.N)
-			jwk.KTY = KeyTypeRSA.String()
-		default:
-			// Skip key.
-			continue
+			return nil, fmt.Errorf("failed to marshal key: %w", err)
 		}
-		jwk.KID = meta.KeyID
 		jwks.Keys = append(jwks.Keys, jwk)
 	}
 
 	return json.Marshal(jwks)
+}
+
+// KeyMarshalOptions are used to specify options for marshalling a JSON Web Key.
+type KeyMarshalOptions struct {
+	EncodePrivate bool
+}
+
+// KeyMarshal transforms a KeyWithMeta into a JWKMarshal, which is used to marshal/unmarshal a JSON Web Key.
+func KeyMarshal(meta KeyWithMeta, options KeyMarshalOptions) (JWKMarshal, error) {
+	var jwk JWKMarshal
+	switch key := meta.Key.(type) {
+	case *ecdsa.PrivateKey:
+		pub := key.PublicKey
+		jwk.CRV = pub.Curve.Params().Name
+		jwk.X = bigIntToBase64RawURL(pub.X)
+		jwk.Y = bigIntToBase64RawURL(pub.Y)
+		jwk.KTY = KeyTypeEC.String()
+		if options.EncodePrivate {
+			jwk.D = bigIntToBase64RawURL(key.D)
+		}
+	case ecdsa.PublicKey:
+		jwk.CRV = key.Curve.Params().Name
+		jwk.X = bigIntToBase64RawURL(key.X)
+		jwk.Y = bigIntToBase64RawURL(key.Y)
+		jwk.KTY = KeyTypeEC.String()
+	case ed25519.PrivateKey:
+		pub := key.Public().(ed25519.PublicKey)
+		jwk.X = base64.RawURLEncoding.EncodeToString(pub)
+		jwk.KTY = KeyTypeOKP.String()
+		if options.EncodePrivate {
+			jwk.D = base64.RawURLEncoding.EncodeToString(key)
+		}
+	case ed25519.PublicKey:
+		jwk.X = base64.RawURLEncoding.EncodeToString(key)
+		jwk.KTY = KeyTypeOKP.String()
+	case *rsa.PrivateKey:
+		pub := key.PublicKey
+		jwk.E = bigIntToBase64RawURL(big.NewInt(int64(pub.E)))
+		jwk.N = bigIntToBase64RawURL(pub.N)
+		jwk.KTY = KeyTypeRSA.String()
+		if options.EncodePrivate {
+			jwk.D = bigIntToBase64RawURL(key.D)
+			jwk.P = bigIntToBase64RawURL(key.Primes[0])
+			jwk.Q = bigIntToBase64RawURL(key.Primes[1])
+			jwk.DP = bigIntToBase64RawURL(key.Precomputed.Dp)
+			jwk.DQ = bigIntToBase64RawURL(key.Precomputed.Dq)
+			jwk.QI = bigIntToBase64RawURL(key.Precomputed.Qinv)
+			for i := 2; i < len(key.Primes); i++ {
+				jwk.OTH = append(jwk.OTH, OtherPrimes{
+					CRTFactorExponent:    bigIntToBase64RawURL(key.Precomputed.CRTValues[i].Exp),
+					CRTFactorCoefficient: bigIntToBase64RawURL(key.Precomputed.CRTValues[i].Coeff),
+					PrimeFactor:          bigIntToBase64RawURL(key.Precomputed.CRTValues[i].R),
+				})
+			}
+		}
+	case rsa.PublicKey:
+		jwk.E = bigIntToBase64RawURL(big.NewInt(int64(key.E)))
+		jwk.N = bigIntToBase64RawURL(key.N)
+		jwk.KTY = KeyTypeRSA.String()
+	default:
+		return JWKMarshal{}, fmt.Errorf("%w: %T", ErrUnsupportedKeyType, key)
+	}
+	jwk.KID = meta.KeyID
+	return jwk, nil
 }
 
 func bigIntToBase64RawURL(i *big.Int) string {
