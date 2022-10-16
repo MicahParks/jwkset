87cdbbcbaa8fc60aa3558b5d3453d6286b8974b5
Micah Parks <gpg@micahparks.com>
2022-10-16 14:18:33 -0400

    Move storage error testing to a separate file

--- jwk_test.go
+++ jwk_test.go
@@ -9,34 +9,11 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
-	"errors"
 	"testing"
 	"time"
 
 	"github.com/MicahParks/jwkset"
 )
-
-var (
-	errStorage = errors.New("storage error")
-)
-
-type storageError struct{}
-
-func (s storageError) DeleteKey(ctx context.Context, keyID string) (ok bool, err error) {
-	return false, errStorage
-}
-
-func (s storageError) ReadKey(ctx context.Context, keyID string) (jwkset.KeyWithMeta, error) {
-	return jwkset.KeyWithMeta{}, errStorage
-}
-
-func (s storageError) SnapshotKeys(ctx context.Context) ([]jwkset.KeyWithMeta, error) {
-	return nil, errStorage
-}
-
-func (s storageError) WriteKey(ctx context.Context, meta jwkset.KeyWithMeta) error {
-	return errStorage
-}
 
 func TestJSON(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -102,19 +79,6 @@ func TestJSON(t *testing.T) {
 		t.Fatalf("Failed to get JSON. %s", err)
 	}
 	compareJSON(t, jsonRepresentation, true)
-}
-
-func TestStorageError(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	jwks := jwkset.NewMemory()
-	jwks.Store = storageError{}
-
-	_, err := jwks.JSONPublic(ctx)
-	if err == nil {
-		t.Fatalf("Expected error, but got none.")
-	}
 }
 
 func compareJSON(t *testing.T, actual json.RawMessage, private bool) {
