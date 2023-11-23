7188c048e42b62e5bee0d8ccf898060657b78b2c
Micah Parks <66095735+MicahParks@users.noreply.github.com>
2023-11-23 08:52:03 -0500

    Add remaining algorithms

--- constants.go
+++ constants.go
@@ -1,34 +1,54 @@
 package jwkset
 
 const (
-	// AlgHS256 is the HMAC using SHA-256 algorithm.
-	AlgHS256 ALG = "HS256"
-	// AlgHS384 is the HMAC using SHA-384 algorithm.
-	AlgHS384 ALG = "HS384"
-	// AlgHS512 is the HMAC using SHA-512 algorithm.
-	AlgHS512 ALG = "HS512"
-	// AlgRS256 is the RSASSA-PKCS1-v1_5 using SHA-256 algorithm.
-	AlgRS256 ALG = "RS256"
-	// AlgRS384 is the RSASSA-PKCS1-v1_5 using SHA-384 algorithm.
-	AlgRS384 ALG = "RS384"
-	// AlgRS512 is the RSASSA-PKCS1-v1_5 using SHA-512 algorithm.
-	AlgRS512 ALG = "RS512"
-	// AlgES256 is the ECDSA using P-256 and SHA-256 algorithm.
-	AlgES256 ALG = "ES256"
-	// AlgES384 is the ECDSA using P-384 and SHA-384 algorithm.
-	AlgES384 ALG = "ES384"
-	// AlgES512 is the ECDSA using P-521 and SHA-512 algorithm.
-	AlgES512 ALG = "ES512"
-	// AlgPS256 is the RSASSA-PSS using SHA-256 and MGF1 with SHA-256 algorithm.
-	AlgPS256 ALG = "PS256"
-	// AlgPS384 is the RSASSA-PSS using SHA-384 and MGF1 with SHA-384 algorithm.
-	AlgPS384 ALG = "PS384"
-	// AlgPS512 is the RSASSA-PSS using SHA-512 and MGF1 with SHA-512 algorithm.
-	AlgPS512 ALG = "PS512"
-	// AlgNone is the No digital signature or MAC performed algorithm.
-	AlgNone ALG = "none"
-	// AlgEdDSA is the EdDSA algorithm.
-	AlgEdDSA ALG = "EdDSA"
+	AlgHS256            ALG = "HS256"
+	AlgHS384            ALG = "HS384"
+	AlgHS512            ALG = "HS512"
+	AlgRS256            ALG = "RS256"
+	AlgRS384            ALG = "RS384"
+	AlgRS512            ALG = "RS512"
+	AlgES256            ALG = "ES256"
+	AlgES384            ALG = "ES384"
+	AlgES512            ALG = "ES512"
+	AlgPS256            ALG = "PS256"
+	AlgPS384            ALG = "PS384"
+	AlgPS512            ALG = "PS512"
+	AlgNone             ALG = "none"
+	AlgRSA1_5           ALG = "RSA1_5"
+	AlgRSAOAEP          ALG = "RSA-OAEP"
+	AlgRSAOAEP256       ALG = "RSA-OAEP-256"
+	AlgA128KW           ALG = "A128KW"
+	AlgA192KW           ALG = "A192KW"
+	AlgA256KW           ALG = "A256KW"
+	Algdir              ALG = "dir"
+	AlgECDHES           ALG = "ECDH-ES"
+	AlgECDHESA128KW     ALG = "ECDH-ES+A128KW"
+	AlgECDHESA192KW     ALG = "ECDH-ES+A192KW"
+	AlgECDHESA256KW     ALG = "ECDH-ES+A256KW"
+	AlgA128GCMKW        ALG = "A128GCMKW"
+	AlgA192GCMKW        ALG = "A192GCMKW"
+	AlgA256GCMKW        ALG = "A256GCMKW"
+	AlgPBES2HS256A128KW ALG = "PBES2-HS256+A128KW"
+	AlgPBES2HS384A192KW ALG = "PBES2-HS384+A192KW"
+	AlgPBES2HS512A256KW ALG = "PBES2-HS512+A256KW"
+	AlgA128CBCHS256     ALG = "A128CBC-HS256"
+	AlgA192CBCHS384     ALG = "A192CBC-HS384"
+	AlgA256CBCHS512     ALG = "A256CBC-HS512"
+	AlgA128GCM          ALG = "A128GCM"
+	AlgA192GCM          ALG = "A192GCM"
+	AlgA256GCM          ALG = "A256GCM"
+	AlgEdDSA            ALG = "EdDSA"
+	// AlgRS1              ALG = "RS1" // Prohibited.
+	AlgRSAOAEP384 ALG = "RSA-OAEP-384"
+	AlgRSAOAEP512 ALG = "RSA-OAEP-512"
+	// AlgA128CBC          ALG = "A128CBC" // Prohibited.
+	// AlgA192CBC          ALG = "A192CBC" // Prohibited.
+	// AlgA256CBC          ALG = "A256CBC" // Prohibited.
+	// AlgA128CTR          ALG = "A128CTR" // Prohibited.
+	// AlgA192CTR          ALG = "A192CTR" // Prohibited.
+	// AlgA256CTR          ALG = "A256CTR" // Prohibited.
+	// AlgHS1              ALG = "HS1"     // Prohibited.
+	AlgES256K ALG = "ES256K"
 
 	// KeyOpsSign is a key operation for signing.
 	KeyOpsSign = "sign"
@@ -75,7 +95,7 @@ const (
 )
 
 // ALG is a set of "JSON Web Signature and Encryption Algorithms" types from
-// https://www.iana.org/assignments/jose/jose.xhtml(JWA) as defined in
+// https://www.iana.org/assignments/jose/jose.xhtml as defined in
 // https://www.rfc-editor.org/rfc/rfc7518#section-7.1
 type ALG string
 
@@ -84,7 +104,11 @@ func (alg ALG) String() string {
 }
 func (alg ALG) valid() bool {
 	switch alg {
-	case AlgHS256, AlgHS384, AlgHS512, AlgRS256, AlgRS384, AlgRS512, AlgES256, AlgES384, AlgES512, AlgPS256, AlgPS384, AlgPS512, AlgNone, AlgEdDSA:
+	case AlgHS256, AlgHS384, AlgHS512, AlgRS256, AlgRS384, AlgRS512, AlgES256, AlgES384, AlgES512, AlgPS256, AlgPS384,
+		AlgPS512, AlgNone, AlgRSA1_5, AlgRSAOAEP, AlgRSAOAEP256, AlgA128KW, AlgA192KW, AlgA256KW, Algdir, AlgECDHES,
+		AlgECDHESA128KW, AlgECDHESA192KW, AlgECDHESA256KW, AlgA128GCMKW, AlgA192GCMKW, AlgA256GCMKW,
+		AlgPBES2HS256A128KW, AlgPBES2HS384A192KW, AlgPBES2HS512A256KW, AlgA128CBCHS256, AlgA192CBCHS384,
+		AlgA256CBCHS512, AlgA128GCM, AlgA192GCM, AlgA256GCM, AlgEdDSA, AlgRSAOAEP384, AlgRSAOAEP512, AlgES256K:
 		return true
 	}
 	return false
